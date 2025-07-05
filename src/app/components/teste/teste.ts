630367abc0d981ed0ce1b2edef912f6d0477526b
khayo.rodrigues <khayovannucci@gmail.com>
2025-07-05 12:16:50 -0400

    deploy

--- src/app/components/teste/teste.ts
+++ src/app/components/teste/teste.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DatePickerModule } from 'primeng/datepicker';
 import { SelectModule } from 'primeng/select';
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-teste',
-  imports: [DatePickerModule, SelectModule, ButtonModule],
+  imports: [DatePickerModule, SelectModule, ButtonModule, InputTextModule, TextareaModule],
   templateUrl: './teste.html',
   styleUrl: './teste.css'
 })
